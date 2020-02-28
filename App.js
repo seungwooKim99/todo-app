a087a796b23bc10347b8e351f7accbe6611c7d7b
seungwooKim99 <sonicdx886@gmail.com>
2020-02-28 15:01:20 +0900

    #To Do + Button Design

--- App.js
+++ App.js
@@ -1,21 +1,44 @@
 import React from 'react';
 import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions,
-Platform } from 'react-native';
+Platform, 
+ScrollView} from 'react-native';
 
+import ToDo from "./ToDo";
 const {height, width} = Dimensions.get("window");
 
 export default class App extends React.Component {
+  state = {
+    newToDo: ""
+  };
+
   render() {
+    const { newToDo } = this.state;
     return (
       <View style={styles.container}>
         <StatusBar barStyle="light-content" /> 
         <Text style={styles.title}>What To Do ?!</Text>
         <View style={styles.card}>
-          <TextInput style={styles.input} placeholder={"New To Do"} />
+          <TextInput 
+            style={styles.input}
+            placeholder={"New To Do"}
+            value={newToDo} 
+            onChangeText={this.controlNewToDo} 
+            placeholderTextColor={"#999"} 
+            returnKeyType={"done"}
+            autoCorrect={false}
+          />
+          <ScrollView contentContainerStyle={styles.toDos}>
+            <ToDo />
+          </ScrollView>
         </View>
       </View>
     );
   }
+  controlNewToDo = text => {
+    this.setState({
+      newToDo: text
+    });
+  };
 }
 
 
@@ -58,5 +81,14 @@ const styles = StyleSheet.create({
         }
       }
     })
+  },
+  input: {
+    padding: 20,
+    borderBottomColor: "#bbb",
+    borderBottomWidth: 1,
+    fontSize: 25
+  },
+  toDos: {
+    alignItems: "center"
   }
 });
---
+++ ToDo.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+import { View, Text, TouchableOpacity, StyleSheet, Dimensions } from "react-native";
+
+const { width, height } = Dimensions.get("window");
+
+export default class ToDo extends Component{
+    state = {
+        isEditing: false,
+        isCompleted: false
+    };
+
+    render(){
+        const {isCompleted} = this.state;
+        return (
+            <View style={styles.container}>
+                <TouchableOpacity onPress={this.toggleComplete}>
+                    <View style={[
+                        styles.circle,
+                        isCompleted ? styles.completedCircle : styles.uncompletedCircle
+                     ]}
+                    />
+                </TouchableOpacity>
+                <Text style={styles.text}>Hello I'm a To Do</Text>
+            </View>
+        );
+    }
+
+    toggleComplete = () => {
+        this.setState(prevState => {
+            return {
+                isCompleted: !prevState.isCompleted
+            };
+        });
+    };
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: width - 50,
+        borderBottomColor: "#bbb",
+        borderBottomWidth: StyleSheet.hairlineWidth,
+        flexDirection: "row",
+        alignItems: "center"
+    },
+    circle: {
+        width: 30,
+        height: 30,
+        borderRadius: 15,
+        borderColor: "red",
+        borderWidth: 3,
+        marginRight: 20
+    },
+    completedCircle: {
+        borderColor: "#bbb"
+    },
+    uncompletedCircle: {
+        borderColor: "#F23567"
+    },
+    text: {
+        fontWeight: "600",
+        fontSize: 20,
+        marginVertical: 20
+    }
+});
